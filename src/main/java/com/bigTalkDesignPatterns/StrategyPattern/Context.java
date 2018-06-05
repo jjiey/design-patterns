78459c6b1c575b38dc7b608fe614fcb052dfcb84
yangjie7 <yangjie7@genomics.cn>
2018-06-05 18:11:35 +0800

    add Strategy+Factory demo

--- src/main/java/com/bigTalkDesignPatterns/StrategyPattern/Context.java
+++ src/main/java/com/bigTalkDesignPatterns/StrategyPattern/Context.java
@@ -16,4 +16,17 @@ public class Context {
 	public void ContextInterface() {
 		this.strategy.algorithmInterface();
 	}
+
+//	//也可以策略与简单工厂结合:增加构造方法如下
+//	Demo demo = null;
+//	public Context(String type) {
+//		switch (type) {
+//			case "1" :
+//				demo = new A();
+//				break;
+//			case "2" :
+//				demo = new B();
+//				break;
+//		}
+//	}
 }
