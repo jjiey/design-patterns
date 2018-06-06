c86c545120e7754afd85fee3adfa7295ab98770d
yangjie7 <yangjie7@genomics.cn>
2018-06-06 16:24:37 +0800

    add ProxyPattern

---
+++ src/main/java/com/bigTalkDesignPatterns/ProxyPattern/Main.java
@@ -0,0 +1,14 @@
+package com.bigTalkDesignPatterns.ProxyPattern;
+
+/**
+ * 单元测试类
+ *
+ * @author yj
+ *
+ */
+public class Main {
+	public static void main(String[] args) {
+		Proxy proxy = new Proxy();
+		proxy.request();
+	}
+}
---
+++ src/main/java/com/bigTalkDesignPatterns/ProxyPattern/Subject.java
@@ -0,0 +1,13 @@
+package com.bigTalkDesignPatterns.ProxyPattern;
+
+/**
+ * Subject类，定义了RealSubject和Proxy的共用接口，这样就在任何使用RealSubject的地饭都可以使用Proxy
+ * 
+ * @author yj
+ * 
+ */
+public abstract class Subject {
+
+	public abstract void request();
+
+}
