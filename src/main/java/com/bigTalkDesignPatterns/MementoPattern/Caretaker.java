4cbadd2ca817e7b63c735b21278c1092d1f63c93
yangjie7 <yangjie7@genomics.cn>
2018-06-11 16:05:37 +0800

    add MementoPattern

---
+++ src/main/java/com/bigTalkDesignPatterns/MementoPattern/Caretaker.java
@@ -0,0 +1,18 @@
+package com.bigTalkDesignPatterns.MementoPattern;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 管理者
+ * 
+ * @author clarck
+ * 
+ */
+@Getter
+@Setter
+public class Caretaker {
+
+	private Memento memento;
+
+}
---
+++ src/main/java/com/bigTalkDesignPatterns/MementoPattern/Memento.java
@@ -0,0 +1,18 @@
+package com.bigTalkDesignPatterns.MementoPattern;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 备忘录
+ * 
+ * @author clarck
+ * 
+ */
+@Getter
+@AllArgsConstructor
+public class Memento {
+
+	private String state;
+
+}
