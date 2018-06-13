7574b5b58a32a0f65f83e4925bbb86459f2034e7
yangjie7 <yangjie7@genomics.cn>
2018-06-13 11:01:00 +0800

    add MediatorPattern

---
+++ src/main/java/com/bigTalkDesignPatterns/MediatorPattern/Colleague.java
@@ -0,0 +1,16 @@
+package com.bigTalkDesignPatterns.MediatorPattern;
+
+import lombok.AllArgsConstructor;
+
+/**
+ * 抽象同事类
+ *
+ * @author yj
+ *
+ */
+@AllArgsConstructor
+public class Colleague {
+
+	protected Mediator mediator;
+
+}
---
+++ src/main/java/com/bigTalkDesignPatterns/MediatorPattern/ConcrateMediator.java
@@ -0,0 +1,25 @@
+package com.bigTalkDesignPatterns.MediatorPattern;
+
+import lombok.Setter;
+
+/**
+ * 具体中介者类
+ *
+ * @author yj
+ *
+ */
+@Setter
+public class ConcrateMediator extends Mediator {
+	private ConcrateColleague1 colleague1;
+	private ConcrateColleague2 colleague2;
+	
+	@Override
+	public void send(String message, Colleague colleague) {
+		if (colleague == colleague1) {
+			colleague2.notify(message);
+		} else {
+			colleague1.notify(message);
+		}
+	}
+	
+}
